fde9c29728e4d01bf6192f6795f3bcc5f6174042
Sami Sieranoja <sami.sieranoja@gmail.com>
2026-03-07 17:11:31 +0200

    refactor: deleted unused functions

--- lib/vimamsa/gui_text.rb
+++ lib/vimamsa/gui_text.rb
@@ -24,26 +24,4 @@ module Gui
       self.hilight_range(bf, r, tag: tag)
     }
   end
-
-  def self.highlight_match_old(bf, str, color: "#aa0000ff")
-    vbuf = bf.view.buffer
-    r = Regexp.new(Regexp.escape(str), Regexp::IGNORECASE)
-
-    hlparts = []
-
-    tt = vma.gui.view.buffer.tag_table.lookup("highlight_match_tag")
-    if tt.nil?
-      tt = vma.gui.view.buffer.create_tag("highlight_match_tag")
-    end
-
-    tt.weight = 650
-    tt.foreground = color
-
-    ind = scan_indexes(bf, r)
-    ind.each { |x|
-      itr = vbuf.get_iter_at(:offset => x)
-      itr2 = vbuf.get_iter_at(:offset => x + str.size)
-      vbuf.apply_tag(tt, itr, itr2)
-    }
-  end
 end
