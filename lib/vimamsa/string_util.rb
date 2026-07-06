59cdb3c5b385de5a81684e18f5140fa8b918a9b5
Sami Sieranoja <sami.sieranoja@gmail.com>
2026-07-06 23:24:15 +0300

    refactor: replace flip_begin_end etc. with a generic flip_term

--- lib/vimamsa/string_util.rb
+++ lib/vimamsa/string_util.rb
@@ -1,77 +1,63 @@
-
 module Vimamsa
-def flip_begin_end(str)
-  str.gsub(/\b(begin|end)\b/i) do |match|
-    if match.match?(/\Abegin\z/i)
-      replacement = "end"
-    else
-      replacement = "begin"
+  def flip_term(str)
+    puts "STR:#{str}"
+    y = str.downcase
+    replacements = [["true", "false"], ["left", "right"], ["begin", "end"], ["spring","summer", "autumn","winter"]]
+    rep = nil
+    for x in replacements
+      i = x.find_index(y)
+      if i
+        i += 1
+        if i >= x.size
+          i = 0
+        end
+        rep = x[i]
+        # rep = (x - [y])[0]
+        if str == str.upcase
+          rep = rep.upcase
+        elsif str[0] == str[0].upcase
+          rep = rep.capitalize
+        else
+          rep = rep.downcase
+        end
+        return rep
+      end
     end
+    return rep
+  end
 
-    if match == match.upcase
-      replacement.upcase
-    elsif match[0] == match[0].upcase
-      replacement.capitalize
-    else
-      replacement.downcase
+  def to_camel_case(str)
+    words = str.split(/\W+/) # Split the input string into words
+    camel_case_words = words.map.with_index do |word, index|
+      index == 0 ? word.downcase : word.capitalize
     end
+    camel_case_words.join
   end
-end
 
-def flip_true_false(str)
-  str.gsub(/\b(true|false)\b/i) do |match|
-    if match.match?(/\Atrue\z/i)
-      replacement = "false"
-    else
-      replacement = "true"
+  # Get all indexes for start of matching regexp
+  def scan_indexes(txt, regex)
+    # indexes = txt.enum_for(:scan, regex).map { Regexp.last_match.begin(0) + 1 }
+    indexes = txt.enum_for(:scan, regex).map { Regexp.last_match.begin(0) }
+    return indexes
+  end
+
+  def is_path(s)
+    m = s.match(/(~[a-z]*)?\/.*\//)
+    if m != nil
+      return true
     end
+    return false
+  end
 
-    # preserve casing style
-    if match == match.upcase
-      replacement.upcase
-    elsif match[0] == match[0].upcase
-      replacement.capitalize
-    else
-      replacement.downcase
+  def sanitize_input(str)
+    if str.encoding != Encoding::UTF_8
+      str = text.encode(Encoding::UTF_8)
     end
+    str.gsub!(/\r\n/, "\n")
+    return str
   end
-end
 
-def to_camel_case(str)
-  words = str.split(/\W+/) # Split the input string into words
-  camel_case_words = words.map.with_index do |word, index|
-    index == 0 ? word.downcase : word.capitalize
+  def is_url(s)
+    return s.match(/(https?|file):\/\/.*/) != nil
   end
-  camel_case_words.join
-end
-
-# Get all indexes for start of matching regexp
-def scan_indexes(txt, regex)
-  # indexes = txt.enum_for(:scan, regex).map { Regexp.last_match.begin(0) + 1 }
-  indexes = txt.enum_for(:scan, regex).map { Regexp.last_match.begin(0) }
-  return indexes
-end
-
-def is_path(s)
-  m = s.match(/(~[a-z]*)?\/.*\//)
-  if m != nil
-    return true
-  end
-  return false
-end
-
-
-def sanitize_input(str)
-  if str.encoding != Encoding::UTF_8
-    str = text.encode(Encoding::UTF_8)
-  end
-  str.gsub!(/\r\n/, "\n")
-  return str
-end
-
-def is_url(s)
-  return s.match(/(https?|file):\/\/.*/) != nil
-end
-
-
 end # module Vimamsa
